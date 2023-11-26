ff62b502355ad5f3cd3ca72889fc9e6d575955a4
susan <jmward01@gmail.com>
2023-11-25 21:47:13 -0800

    added the attn norm experiments.

--- lmplay/__init__.py
+++ lmplay/__init__.py
@@ -1,1 +1,2 @@
-from lmplay.exp.embeddings.unified_embeddings_v1_0_1.model import ModelRunner as CurrentExpModelRunner
+from lmplay.exp.embeddings.unified_embeddings_v1_0.model import ModelRunner as CurrentExpModelRunner
+#from lmplay.exp.attn_norm.normv.model import ModelRunner as CurrentExpModelRunner
