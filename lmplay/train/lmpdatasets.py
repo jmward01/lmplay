98e69a2645fbf62975fbfd45af8f953e22ff4a3b
susan <jmward01@gmail.com>
2024-12-30 14:46:52 -0800

    datasets are now looked up in the out_gpt dir. This is to make it easier to copy them to other servers instead of downloading them.

--- lmplay/train/lmpdatasets.py
+++ lmplay/train/lmpdatasets.py
@@ -1,7 +1,17 @@
-from datasets import load_dataset
+from datasets import load_dataset, load_from_disk
+import os
+DATASET_DIR = "out_gpt/datasets"
 
 def _get_wikipedia(seed:int, val_split:float, date="20231101", lang="en"):
-  ds = load_dataset("wikimedia/wikipedia", f"{date}.{lang}")['train']
+  save_name = os.path.expanduser(f"{DATASET_DIR}/wikimedia_wikipedia_{date}_{lang}.hf")
+  if not os.path.exists(save_name):
+    print(f"{save_name} not found. Downloading from hf.")
+    os.makedirs(DATASET_DIR, exist_ok=True)
+    ds = load_dataset("wikimedia/wikipedia", f"{date}.{lang}")['train']
+    ds.save_to_disk(save_name)
+    print(f"{save_name} saved")
+  else:
+    ds = load_from_disk(save_name)
   datasets = ds.train_test_split(test_size=val_split, seed = seed)
   return {'train':datasets['train'], 'validation':datasets['test']}
 
