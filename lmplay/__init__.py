0d7e0c2cef45caef7036ec42918e8139a3a354cf
susan <jmward01@gmail.com>
2025-01-02 10:34:05 -0800

    added sac 1.1 16x front end emb on GPU so it needs a big GPU to run.

--- lmplay/__init__.py
+++ lmplay/__init__.py
@@ -7,6 +7,7 @@ from lmplay.exp.embeddings.unified_embeddings_v1_3.model import ModelRunner as U
 from lmplay.exp.embeddings.unified_embeddings_v2_0.model import ModelRunner as UE8x2_0ModelRunner
 from lmplay.exp.combined.sacrificial.sac1_0.model import ModelRunner as SAC1_0ModelRunner
 from lmplay.exp.combined.sacrificial.sac1_0_1.model import ModelRunner as SAC1_0_1ModelRunner
+from lmplay.exp.combined.sacrificial.sac1_1.model import ModelRunner as SAC1_1ModelRunner
 from lmplay.exp.attn_norm.normv.model import ModelRunner as NormVModelRunner
 from lmplay.base.encoder.model import ModelRunner as GPT2ishModelRunner
 from lmplay.base.base_model import LMRunnerBase
@@ -18,6 +19,7 @@ MODEL_RUNNERS = dict(gpt2ish=GPT2ishModelRunner,
                      normv=NormVModelRunner,
                      sac1_0=SAC1_0ModelRunner,
                      sac16x1_0=SAC1_0_1ModelRunner,
+                     sac16x1_1=SAC1_1ModelRunner,
                      ue8x=UE8xModelRunner,
                      ue8x1=UE8x1ModelRunner,
                      ue16x=UE16xModelRunner,
