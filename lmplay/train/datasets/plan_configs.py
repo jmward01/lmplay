5d9485d40b2502d04a3763302839985c3ce5d43e
susan <jmward01@gmail.com>
2025-01-19 22:44:26 -0800

    updated the full_v2 training plan

--- lmplay/train/datasets/plan_configs.py
+++ lmplay/train/datasets/plan_configs.py
@@ -28,7 +28,7 @@ PRETRAIN_STEP = {'datasets': ['tinystories'],
                  'epochs': 1.0,
                  'step_name': 'pretrain'}
 BIG_PRETRAIN_STEP = {'datasets': ['tinystories', 'cosmopedia_v2'],
-                 'epochs': 1.0,
+                 'epochs': 0.1,
                  'step_name': 'pretrain'}
 
 INSTRUCTION_FT_STEP = {'datasets': ['openorca', 'orcamathword200', 'smoltalk_all'],
@@ -64,7 +64,7 @@ FULL_V1 = {'datasets': ALL_DATASETS,
 FULL_V2 = {'datasets': ALL_DATASETS,
            'seed': 0,
            'steps': [WIKI_EN_ES_STEP,
-                     PRETRAIN_STEP,
+                     BIG_PRETRAIN_STEP,
                      INSTRUCTION_FT_STEP]}
 
 
