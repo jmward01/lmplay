e18ca1fd07aae292c0062e94697e730b65adba74
susan <jmward01@gmail.com>
2025-01-31 22:58:58 -0800

    nn memory coming to the party.....

--- lmplay/exp/nnmem/v1/modules.py
+++ lmplay/exp/nnmem/v1/modules.py
@@ -22,14 +22,16 @@ class Block(nn.Module):
     super().__init__()
     if ln_attn:
       self.ln1 = nn.LayerNorm(embed_dim)
-      self.ln_cross = nn.LayerNorm(embed_dim)
+      self.ln1_cross = nn.LayerNorm(embed_dim)
     else:
       self.ln1 = lambda x:x
-      self.ln_cross = lambda x:x
+      self.ln1_cross = lambda x:x
     if ln_mlp:
       self.ln2 = nn.LayerNorm(embed_dim)
+      self.ln2_cross = nn.LayerNorm(embed_dim)
     else:
       self.ln2 = lambda x:x
+      self.ln2_cross = lambda x:x
     if isinstance(nnm, int):
       self.nnm = UnifiedEmbedding(nnm, embed_dim, front_emb_mul)
       nnm = self.nnm
@@ -60,10 +62,17 @@ class Block(nn.Module):
                             create_linear(linear, 'block_ff_2', embed_dim * 4, embed_dim),
                             nn.Dropout(ff_dropout))
 
+    self.ff_cross = nn.Sequential(create_linear(linear, 'block_ff_1', embed_dim, embed_dim * 4),
+                            nn.GELU(),
+                            create_linear(linear, 'block_ff_2', embed_dim * 4, embed_dim),
+                            nn.Dropout(ff_dropout))
+
+
   def forward(self, x, cache:Optional[list]=None):
     #A simple 'block' that uses residual connections and gives attn + pure logic both a chance to modify the hidden layer
     #the 'cache' is the kv cache and is only needed for inference, not training.
-    x = x + self.x_attn(self.ln_cross(x), x_cross=self._nnm[0]())
+    x = x + self.x_attn(self.ln1_cross(x), x_cross=self._nnm[0]())
+    x = x + self.ff_cross(self.ln2_cross(x))
     x = x + self.attn(self.ln1(x), cache=cache)
     x = x + self.ff(self.ln2(x))
     return x
