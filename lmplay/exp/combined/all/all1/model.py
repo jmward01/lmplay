f818aba6ee82d2f8cf1b14ac605441dae64cb033
susan <jmward01@gmail.com>
2025-02-22 15:54:36 -0800

    changed all1_0

--- lmplay/exp/combined/all/all1/model.py
+++ lmplay/exp/combined/all/all1/model.py
@@ -5,15 +5,15 @@ from typing import Optional
 from lmplay.modules import Block
 import tiktoken
 from lmplay.base.base_model import LMBase
-from lmplay.modules import SDULinear, ULinear, UnifiedEmbedding, MultiMLP, accepts_purpose
+from lmplay.modules import ULinear
+from lmplay.modules import UnifiedEmbedding, ConvertableEmbedding
+from lmplay.exp.weights.unified_weights_v2_1.modules import SULinear
 from functools import partial
-
 
 def _p(v) -> str:
   if v is None:
     return 'N'
   return str(int(v))
-
 
 class GPT2(LMBase):
   def __init__(self,
@@ -24,92 +24,42 @@ class GPT2(LMBase):
                attn_dropout: Optional[float] = 0.1,
                ff_dropout: Optional[float] = 0.1,
                embed_dropout: Optional[float] = 0.1,
-               front_embed_mul=16.0,
-               exp_mul=64.0,
+               front_embed_mul=16,
                for_train=True,
                keep_embed_on_cpu=False,
-               ln_attn=False,  # UW get a big boost from this and it is fewer parameter/computation so not cheating!
-               ln_mlp=False,  # UW get a big boost from this and it is fewer parameter/computation so not cheating!
-               ue_sduw=False,
-               t_sduw=True,
-               ignore_purpose=True,
-               dl_fc=False,
-               max_predict=False,
-               mha_lradd=True,
-               ff_lradd=True,
-               norm_v=True,
-               version="1",
+               sulinear=True,
+               version="all_1",
                **ignore):
-    # Second in the 'sacrificial' line of experiments. These models combine all the sacrificial experiments, experiments that train with extra parameters that are removed for prod.
+    # First in the 'sacrificial' line of experiments. These models combine all the sacrificial experiments, experiments that train with extra parameters that are removed for prod.
     # This model could be re-saved after training back to a 'standard' version compatible with the gpt2ish baseline weights.
-    # This specific version combines the changes from unified embeddings 1.3 (sort of) and unified weights 2.1
-    super().__init__(
-      f"{version}_{_p(ln_attn)}{_p(ln_mlp)}{_p(ue_sduw)}{_p(t_sduw)}{_p(ignore_purpose)}{_p(dl_fc)}{_p(max_predict)}{_p(mha_lradd)}{_p(ff_lradd)}{_p(norm_v)}_{num_blocks}L_{max_len}",
-      max_len=max_len,
-      num_heads=num_heads,
-      num_blocks=num_blocks,
-      embed_dim=embed_dim,
-      attn_dropout=attn_dropout,
-      ff_dropout=ff_dropout,
-      embed_dropout=embed_dropout,
-      front_embed_mul=front_embed_mul,
-      exp_mul=exp_mul,
-      for_train=for_train,
-      keep_embed_on_cpu=keep_embed_on_cpu,
-      ln_attn=ln_attn,
-      ln_mlp=ln_mlp,
-      version=version,
-      ue_sduw=ue_sduw,
-      t_sduw=t_sduw,
-      ignore_purpose=ignore_purpose,
-      dl_fc=dl_fc,
-      max_predict=max_predict,
-      mha_lradd=mha_lradd,
-      ff_lradd=ff_lradd,
-      norm_v=norm_v,
-      **ignore)
-    if max_predict == True:
-      max_predict_size = embed_dim*4
-    else:
-      max_predict_size = None
+    # This specific version combines the changes from unified embeddings 1.3 and unified weights 1.0
+    super().__init__(f"{version}_{front_embed_mul}_{_p(sulinear)}_{num_blocks}L_{max_len}",
+                     max_len=max_len,
+                     num_heads=num_heads,
+                     num_blocks=num_blocks,
+                     embed_dim=embed_dim,
+                     attn_dropout=attn_dropout,
+                     ff_dropout=ff_dropout,
+                     embed_dropout=embed_dropout,
+                     front_embed_mul=front_embed_mul)
+    # same as 1.0 but the linear is now a ULinear!
     keep_embed_on_cpu = for_train and keep_embed_on_cpu
     self.tokenizer = tiktoken.get_encoding("gpt2")
     vocab_size = self.tokenizer.n_vocab
-    expansion_size = int(exp_mul * embed_dim)
-    # self.shared_net = SimpleMLP(expansion_size, embed_dim, layers=2, bias=False, linear=ULinear)
-    self.shared_net = MultiMLP(expansion_size, embed_dim, last_activation=False, layers=0)
+    if sulinear:
+      self.shared_mid_weights = nn.Linear(embed_dim, embed_dim)
+      #This version combines UW 1.0 and UW 2.0
+      linear = partial(SULinear, self.shared_mid_weights)
+    else:
+      linear = ULinear
+
     self.max_len = max_len
-    if t_sduw == True or ue_sduw == True:
-      linear = partial(SDULinear,
-                       share_in=self.shared_net,
-                       share_out=self.shared_net,
-                       exp_mul=exp_mul,
-                       linear=ULinear,
-                       ignore_purpose=ignore_purpose,
-                       cacheable=True,
-                       max_predict_size=max_predict_size)
-      linear = accepts_purpose(linear)
+    if not for_train:
+      # this will convert any UE into a normal embedding. After this, if the model is saved, it can be re-loaded by the baseline model.
+      self.tok_embed = ConvertableEmbedding(vocab_size, embed_dim, front_embed_mul)
     else:
-      linear = nn.Linear
-    if ue_sduw == True:
-      tok_linear = linear
-    elif ue_sduw == False:
-      tok_linear = ULinear
-    else:
-      tok_linear = nn.Linear
-
-    if t_sduw == True:
-      t_linear = linear
-    elif ue_sduw == False:
-      t_linear = ULinear
-    else:
-      t_linear = nn.Linear
-
-    self.tok_embed = UnifiedEmbedding(vocab_size,
-                                      embed_dim,
-                                      front_embed_mul,
-                                      keep_embed_on_cpu=keep_embed_on_cpu,
-                                      linear=tok_linear)
+      self.tok_embed = UnifiedEmbedding(vocab_size, embed_dim, front_embed_mul, keep_embed_on_cpu=keep_embed_on_cpu,
+                                        linear=linear)
     self.pos_embed = nn.Parameter(torch.zeros(1, max_len, embed_dim))
     self.dropout = nn.Dropout(embed_dropout)
     self.blocks = nn.Sequential(*[Block(max_len,
@@ -117,31 +67,11 @@ class GPT2(LMBase):
                                         embed_dim,
                                         attn_dropout=attn_dropout,
                                         ff_dropout=ff_dropout,
-                                        linear=t_linear,
-                                        ln_attn=ln_attn,
-                                        ln_mlp=ln_mlp,
-                                        norm_v=norm_v,
-                                        mha_lradd=mha_lradd,
-                                        ff_lradd=ff_lradd) for _ in range(num_blocks)])
+                                        linear=linear,
+                                        mha_lradd=True,
+                                        ff_lradd=True) for _ in range(num_blocks)])
     self.ln = nn.LayerNorm(embed_dim)
-    if dl_fc == True:
-      self.fc = SDULinear(embed_dim,
-                          vocab_size,
-                          exp_mul=exp_mul,
-                          predict_bias=False,
-                          predict_mbias=True,
-                          predict_mbias2=False,
-                          predict_mbias_a=None,
-                          predict_mbias2_a=None,
-                          share_in=self.shared_net,
-                          share_out=self.shared_net,
-                          linear=linear,
-                          purpose="fc",
-                          ignore_purpose=ignore_purpose,
-                          cacheable=False,
-                          max_predict_size=max_predict_size)
-    else:
-      self.fc = ULinear(embed_dim, vocab_size)
+    self.fc = linear(embed_dim, vocab_size)
 
   def forward(self, x: torch.Tensor, cache: Optional[list] = None):
     seq_len = x.size(1)
