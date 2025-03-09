a7a5aafd1e7202c025a9ebdf168f2c852cac8dab
susan <jmward01@gmail.com>
2025-03-09 12:31:33 -0700

    foc1_1

--- lmplay/exp/focus/v1/versions.py
+++ lmplay/exp/focus/v1/versions.py
@@ -5,7 +5,7 @@ from .model import GPT2
 from lmplay.base.runner_list import expose_runner
 
 
-
+#not really sure what I was thinking here. Meh! Try things and don't think about them too hard unless they work!
 @expose_runner('foc1_0',
                description='Trying a simple idea to allow the model to learn to focus attn.')
 def gpt2ish(*args, **kwargs):
@@ -14,7 +14,7 @@ def gpt2ish(*args, **kwargs):
                           overrides=dict(),
                           **kwargs)
 
-
+#Much simpler thing here. If scaling is really important than allowing it to float to a good value is possibly a good thing.
 @expose_runner('foc1_1',
                description='Trying a less expensive version of this idea.')
 def gpt2ish(*args, **kwargs):
