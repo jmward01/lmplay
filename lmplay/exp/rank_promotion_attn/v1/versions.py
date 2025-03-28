32cc578ccf41e14c1222c1ebd5101f12ee817353
susan <jmward01@gmail.com>
2025-03-28 10:18:01 -0700

    rpa 1.4. smaller

--- lmplay/exp/rank_promotion_attn/v1/versions.py
+++ lmplay/exp/rank_promotion_attn/v1/versions.py
@@ -44,4 +44,13 @@ def runner(*args, **kwargs):
 
                           **kwargs)
 
-#num_distil_head_groups
+@expose_runner('rpa1_4',
+               description='Trying to make things smaller so they are faster. Seeing how small I can make the key dim.')
+def runner(*args, **kwargs):
+  return BasicModelRunner(GPT2,
+                          *args,
+                          overrides=dict(key_dim=12*3,
+                                         num_distil_heads=12,
+                                         num_distil_head_groups=3),
+
+                          **kwargs)
