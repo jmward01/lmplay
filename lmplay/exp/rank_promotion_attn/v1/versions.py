e657f7993e852fde810decdbc6e2cd53954b4ae8
susan <jmward01@gmail.com>
2025-04-05 08:43:04 -0700

    more playing with direct distillation

--- lmplay/exp/rank_promotion_attn/v1/versions.py
+++ lmplay/exp/rank_promotion_attn/v1/versions.py
@@ -177,12 +177,11 @@ def runner(*args, **kwargs):
   return BasicModelRunner(GPT2,
                           *args,
                           overrides=dict(key_dim=12*3,
-                                         attn_scales=((3, 5, 7), (7, 11, 13), (3, 5, 7), (7, 11, 13), (3, 5, 7), (7, 11, 13)),
+                                         attn_scales=((3, 5, 7), (6, 10, 14), (3, 5, 7), (6, 10, 14), (3, 5, 7), (6, 10, 14)),
                                          num_distil_heads=None, #direct distil
                                          add_model_attn=False,
                                          add_attn_position=True, #Makes a difference with direct distil. Probably makes a bigger diff with larger initial scale windows
-                                         intermediate_mul=0,
-                                         utility_intermediate_mul=50,
-                                         layer_proj="R"),
+                                         intermediate_mul=3,
+                                         utility_intermediate_mul=50),
 
                           **kwargs)
